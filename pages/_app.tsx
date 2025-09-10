226fc1af1706a2db0c9e9d076644182f19bba464
agentscharles-web <agentscharles@gmail.com>
2025-09-10 09:22:59 +0100

    Create _app.tsx

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+```tsx
+import type { AppProps } from 'next/app'
+import '@/styles/globals.css'
+
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+return <Component {...pageProps} />
+}
+```
