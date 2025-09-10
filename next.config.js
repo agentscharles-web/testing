faf53c265407e9786ef9368711166d707ed9f0a1
agentscharles-web <agentscharles@gmail.com>
2025-09-10 09:21:37 +0100

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,14 @@
+```js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+images: {
+remotePatterns: [
+{ protocol: 'https', hostname: 'images.igdb.com' }
+]
+}
+}
+module.exports = nextConfig
+```
+
+
+---
